835f1d277fcfe9ade01091a63196f1f103705944
Ivan Sholana <ivansholana@gmail.com>
2024-09-15 20:20:37 +0700

    fix logger

---
+++ main.py
@@ -0,0 +1,1 @@
+from src.CNNChickenClassifier import logger
--- template.py
+++ template.py
@@ -1,40 +1,41 @@
-import setuptools
+import os
+from pathlib import Path
+import logging
 
-# Read the README file for long description
-with open("README.md", "r", encoding="utf-8") as f:
-    long_description = f.read()
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-# Read the requirements file for dependencies
-with open("requirements.txt", "r", encoding="utf-8") as f:
-    requirements = f.readlines()
+project_name = 'CNNChickenClassifier'
 
-__version__ = '0.1.0'
+list_of_files = [
+    '.github/workflows/.gitkeep',
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html",
+    "main.py"
+]
 
-REPO_NAME = 'Chicken-Disease-Classification'
-AUTHOR_USER_NAME = 'IvanSholana'
-SRC_REPO = 'Chicken-Disease-Classification'
-AUTHOR_EMAIL = 'ivansholana@gmail.com'
-
-setuptools.setup(
-    name=SRC_REPO,
-    version=__version__,
-    author=AUTHOR_USER_NAME,
-    author_email=AUTHOR_EMAIL,
-    description="A small python package for CNN app",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
-    project_urls={
-        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
-    },
-    package_dir={"": "src"},
-    packages=setuptools.find_packages(where="src"),
-    install_requires=[req.strip() for req in requirements],  # Reading from requirements.txt
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: MIT License",
-        "Operating System :: OS Independent",
-        "Development Status :: 3 - Alpha",
-    ],
-    python_requires=">=3.9",
-)
+for filepath in list_of_files:
+    filepath = Path(filepath)  # Using pathlib.Path
+    
+    # Create directory if it doesn't exist
+    if filepath.parent != Path('.'):
+        filepath.parent.mkdir(parents=True, exist_ok=True)
+        logging.info(f"Creating directory: {filepath.parent} for the file {filepath.name}")
+    
+    # Create file if it doesn't exist or is empty
+    if not filepath.exists() or filepath.stat().st_size == 0:
+        filepath.touch()  # Create empty file
+        logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File already exists: {filepath}")
